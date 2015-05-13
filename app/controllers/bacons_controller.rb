8bdfff64d37ae9e88907146f9aa6bfecd6a8b309
Felix Krause <KrauseFx@gmail.com>
2015-05-13 16:01:58 +0100

    Added graphs per day view

--- app/controllers/bacons_controller.rb
+++ app/controllers/bacons_controller.rb
@@ -53,4 +53,56 @@ class BaconsController < ApplicationController
       { value: 0.0, color: 'green' }
     ]
   end
+
+  def ran
+    Random.rand(255)
+  end
+
+  def graphs
+    @data = {}
+    @days = []
+    start_time = Time.at(1428883200) # the first day
+
+    Bacon.all.order(:launch_date).each do |bacon|
+      random_color = "rgba(#{ran}, #{ran}, #{ran}, 1.0)"
+      @data[bacon.action_name] ||= {
+        label: bacon.action_name,
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: random_color,
+        pointColor: random_color,
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: []
+      }
+
+      counter = (bacon.launch_date.to_date - start_time.to_date).to_i
+
+      @data[bacon.action_name][:data][counter] ||= 0
+      @data[bacon.action_name][:data][counter] += bacon.launches
+
+      # Fill nils with 0
+      @data[bacon.action_name][:data].each_with_index do |k, index|
+        @data[bacon.action_name][:data][index] ||= 0
+      end
+
+      formatted_string = bacon.launch_date.strftime("%d.%m.%Y")
+      @days << formatted_string unless @days.include?formatted_string
+    end
+
+    # Sort by # of launches
+    @data = @data.sort_by { |name, data| data[:data].sum }.reverse
+
+    # Now generate cumulative graph
+    @cumulative = []
+    @data.each do |key, current|
+      new_val = current.dup
+      new_data = []
+      new_val[:data].each_with_index do |value, i|
+        new_data[i] = value + (new_data[-1] || 0)
+      end
+      new_val[:data] = new_data
+      @cumulative << new_val
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :bacons
 
   root to: 'bacons#stats'
+  get 'graphs' => 'bacons#graphs'
   post 'did_launch' => 'bacons#did_launch'
 end
