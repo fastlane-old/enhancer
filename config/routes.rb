cdac23e1a260644becd19e584b93a12fe44cd107
Michael Furtak <mfurtak@google.com>
2017-04-26 16:30:35 -0400

    Fix healthcheck route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :bacons
 
   root to: 'bacons#stats'
-  get 'admin/ok' => 'healthcheck#index'
+  match 'admin/ok' => 'healthcheck#index'
   get 'index' => 'bacons#stats'
   post 'did_launch' => 'bacons#did_launch'
   get 'stability' => 'stability#index'
